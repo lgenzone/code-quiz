74acf42ced9f25c7bae535fd38345fd506031ed7
lgenzone <lgenzone@icloud.com>
2022-11-03 23:21:31 -0600

    update css
    changed background

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+
+// DOM variables
+var questionsEl = document.querySelector("#questions");
+
+
+
+function startQuiz() {
+    // invisible start screen
+    var startScreenEl = document.getElementById("intro");
+    startScreenEl.setAttribute("class", "hide");}
+
+
+// timer 
+// get help!! 
+
+//question bank
+const questionBank = [
+
+]
