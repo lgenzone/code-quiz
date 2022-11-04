e16a41874c11c99a2dd5290532df39b3b9690c21
lgenzone <lgenzone@icloud.com>
2022-11-04 15:30:57 -0600

    update javascript
    added question & answer templates

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,33 @@
 var  time = 60;
 var timeRemaining = "";
 var timeId;
+var answerA = document.getElementById("answer-a");
+var answerB = document.getElementById("answer-b");
+var answerC = document.getElementById("answer-c");
+var answerD = document.getElementById("answer-d");
+var question = document.getElementById("question");
 
 // DOM variables
 
 var timerEl = document.getElementById("time");
 var startEl = document.getElementById("start");
+
+
+// questions Array - create objects for each question
+var questionsArray = [
+    {
+        question: 'What HTML element do we put the javascript?',
+        answers: ["<Javascript>", "<Script>", "<Scripting>", "<Js>" ],
+        correctAnswer: "<Script>",
+    },
+    { 
+    question: 'What HTML element do we put the javascript?',
+    answers: ["<Javascript>", "<Script>", "<Scripting>", "<Js>" ],
+    correctAnswer: "<Script>",
+    }
+
+   ]
+
 
 startEl.addEventListener("click", function(){
     console.log("button was clicked");
@@ -45,11 +67,53 @@ function endQuiz() {
 
 }
 
+
+
+var questionIndex = 0;
 function getQuestions() { 
+    var currentQuestion = questionsArray[questionIndex];
+    question.textContent = currentQuestion.question;
+    
+    answerA.textContent = questionsArray[questionIndex].answers[0];
+    answerB.textContent = questionsArray[questionIndex].answers[1];
+    answerC.textContent = questionsArray[questionIndex].answers[2];
+    answerD.textContent = questionsArray[questionIndex].answers[3];
 
 }
 
-//question bank
-const questionBank = [
+function checkAnswer(selected) {
+    var correctChoice = questionsArray[questionIndex].correctAnswer;
+    // conditional statement 
+    if(selected === correctChoice) {
+        //tells user whether or not they answered correctly
+        alert("You are correct!");
 
-]
+    } else {
+        alert("Wrong");
+        time -= 5;
+
+    
+    
+    };
+
+    if (questionIndex === questionsArray.length -1) {
+        endQuiz();
+    } else {
+        questionIndex++;
+        getQuestions();
+
+};
+
+answerA.addEventListener("click", function(){
+    checkAnswer(answerA.textContent);
+ });
+ answerB.addEventListener("click", function(){
+    checkAnswer(answerB.textContent);
+ });
+ answerC.addEventListener("click", function(){
+    checkAnswer(answerC.textContent);
+ });
+ answerD.addEventListener("click", function(){
+    checkAnswer(answerD.textContent);
+ });}
+
