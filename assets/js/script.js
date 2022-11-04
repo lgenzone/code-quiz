f27fbd2cb8b972c62f5413ac2b38c38ad8969e09
lgenzone <lgenzone@icloud.com>
2022-11-04 12:34:34 -0600

    update javascript
    added timer and questions page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,53 @@
-
+// variables
+var  time = 60;
+var timeRemaining = "";
+var timeId;
 
 // DOM variables
-var questionsEl = document.querySelector("#questions");
 
+var timerEl = document.getElementById("time");
+var startEl = document.getElementById("start");
+
+startEl.addEventListener("click", function(){
+    console.log("button was clicked");
+    startQuiz();
+});
 
 
 function startQuiz() {
     // invisible start screen
     var startScreenEl = document.getElementById("intro");
-    startScreenEl.setAttribute("class", "hide");}
+    startScreenEl.setAttribute("class", "hide");
+    var questionsEl = document.getElementById("questions");
+    questionsEl.removeAttribute("class");
+    timeRemaining = time;
+    timeId = setInterval(startTimer, 1000);
+    timerEl.textContent = time;
+
+    getQuestions();
+}
 
 
-// timer 
-// get help!! 
+// start timer 
+var startTimer = function() {
+    time--;
+    timerEl.textContent = time;
+
+    if(time <= 0) {
+        endQuiz();
+    }
+
+}
+
+function endQuiz() {
+    // hide questions div and show main scores
+    clearInterval();
+
+}
+
+function getQuestions() { 
+
+}
 
 //question bank
 const questionBank = [
