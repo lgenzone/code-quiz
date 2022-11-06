6b574254039031ab8ed5f1784df025e29df94eb1
lgenzone <lgenzone@icloud.com>
2022-11-06 00:43:30 -0600

    update js
    re-working questions section

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,14 @@
 var  time = 60;
 var timeRemaining = "";
 var timeId;
+var questionIndex = 0;
 var answerA = document.getElementById("answer-a");
 var answerB = document.getElementById("answer-b");
 var answerC = document.getElementById("answer-c");
 var answerD = document.getElementById("answer-d");
+var answerE = document.getElementById("answer-e");
 var question = document.getElementById("question");
+var answersEl = document.querySelector("#answers");
 
 // DOM variables
 
@@ -17,37 +20,111 @@ var startEl = document.getElementById("start");
 // questions Array - create objects for each question
 var questionsArray = [
     {
-        question: 'What HTML element do we put the javascript?',
-        answers: ["<Javascript>", "<Script>", "<Scripting>", "<Js>" ],
-        correctAnswer: "<Script>",
+        question: 'Inside which HTML element do we put the JavaScript?',
+        answers: ["<JavaScript>", "<Script>", "<Scripting>", "<Js>"],
+        correctAnswer: "<Script>"
     },
-    { 
-        question: 'What HTML element do we put the javascript?',
-        answers: ["<Javascript>", "<Script>", "<Scripting>", "<Js>" ],
-        correctAnswer: "<Script>",
+    {
+        question: 'Where is the correct place to insert a JavaScript?',
+        answers: ["The <head> section", "Both the <head> section and the <body> section are correct", "The <body> section"],
+        correctAnswer: "Both the <head> section and the <body> section are correct"
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        answers: ["function myFunction()", "function:myFunction()",  "function = myFunction()"],
+        correctAnswer: "function myFunction()"
+    },
+    {
+        question: "How to write an IF statement in JavaScript?",
+        answers: ["if (i == 5)", "if i = 5", "if i = 5 then", "if i == 5 then"],
+        correctAnswer: "if (i==5)"
+    },
+    {
+        question: "How does a FOR loop start?",
+        answers: ["for (i <= 5; i++)", "for (i = 0; i <= 5; i++)", "for i = 1 to 5", "for (i = 0; i <= 5)"],
+        correctAnswer: "for (i = 0; i <= 5; i++)"
+        
     }
+]
 
-   ]
-
-
+// start quiz by clicking "Start Quiz!"
 startEl.addEventListener("click", function(){
-    console.log("button was clicked");
     startQuiz();
-});
+}); 
 
 
 function startQuiz() {
-    // invisible start screen
+    // displays questions page and hides intro page
     var startScreenEl = document.getElementById("intro");
     startScreenEl.setAttribute("class", "hide");
-    var questionsEl = document.getElementById("questions");
+    var questionsEl = document.getElementById("questions-page");
     questionsEl.removeAttribute("class");
     timeRemaining = time;
+    // start timer
     timeId = setInterval(startTimer, 1000);
     timerEl.textContent = time;
 
     getQuestions();
 }
+
+/* var questionIndex = 0;
+function getQuestions() { 
+    var currentQuestion = questionsArray[questionIndex];
+    question.textContent = currentQuestion.question;
+    
+    answerA.textContent = questionsArray[questionIndex].answers[0];
+    answerB.textContent = questionsArray[questionIndex].answers[1];
+    answerC.textContent = questionsArray[questionIndex].answers[2];
+    answerD.textContent = questionsArray[questionIndex].answers[3];
+    answerE.textContent = questionsArray[questionIndex].answers[4];
+
+  // prompts new question
+  var questEl = document.getElementById("question");
+  questEl.textContent = currentQuestion.question;
+
+} */
+
+var questionIndex = 0;
+function getQuestions() {
+    var currentQuestion = questionsArray[questionIndex];
+
+    var questEl = document.getElementById("question");
+    questEl.textContent = currentQuestion.question;
+
+    answersEl.innerHTML = "";
+
+    currentQuestion.answers.forEach(function(answer, i) {
+        var choiceClick = document.createElement("button");
+        choiceClick.setAttribute("class", "answer");
+        choiceClick.textContent = (i + 1 + ". " + answer);
+
+        choiceClick.onclick = checkAnswer;
+
+        answersEl.appendChild(choiceClick);
+    });
+
+}
+
+
+/* function checkAnswer(selected) {
+    var correctChoice = questionsArray[questionIndex].correctAnswer;
+    // conditional statement 
+    if(selected === correctChoice) {
+        //tells user whether or not they answered correctly
+        alert("You are correct!");
+
+    } else {
+        alert("Wrong");
+        time -= 5;
+    };
+
+    if (questionIndex === questionsArray.length -1) {
+        endQuiz();
+    } else {
+        questionIndex++;
+        getQuestions();
+
+}; */
 
 
 // start timer 
@@ -68,41 +145,6 @@ function endQuiz() {
 }
 
 
-
-var questionIndex = 0;
-function getQuestions() { 
-    var currentQuestion = questionsArray[questionIndex];
-    question.textContent = currentQuestion.question;
-    
-    answerA.textContent = questionsArray[questionIndex].answers[0];
-    answerB.textContent = questionsArray[questionIndex].answers[1];
-    answerC.textContent = questionsArray[questionIndex].answers[2];
-    answerD.textContent = questionsArray[questionIndex].answers[3];
-
-}
-
-function checkAnswer(selected) {
-    var correctChoice = questionsArray[questionIndex].correctAnswer;
-    // conditional statement 
-    if(selected === correctChoice) {
-        //tells user whether or not they answered correctly
-        alert("You are correct!");
-
-    } else {
-        alert("Wrong");
-        time -= 5;
-
-    
-    
-    };
-
-    if (questionIndex === questionsArray.length -1) {
-        endQuiz();
-    } else {
-        questionIndex++;
-        getQuestions();
-
-};
 
 // event listeners
 answerA.addEventListener("click", function(){
