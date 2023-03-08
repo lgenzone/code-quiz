b7eb8e99c8206fb30f5f74bee4bbd72ba130e21a
lgenzone <113480175+lgenzone@users.noreply.github.com>
2023-03-07 23:46:42 -0800

    updated js/debugged/re-wrote

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,6 @@
-// variables
-let time = 60;
-var timeRemaining = "";
-var timeId;
-var questionIndex = 0;
-var userInitials = "";
+const startEl = document.getElementById("start");
 
-// DOM variables
-var timerEl = document.getElementById("time");
-var startEl = document.getElementById("start");
-var question = document.getElementById("question");
-var questionsEl = document.querySelector("questions");
-var answersEl = document.querySelector("#answers");
-var notifyEl = document.querySelector("#notify-user");
-
-
-// questions Array - create objects for each question
+// questions array - create objects for each question
 var questionsArray = [
     {
         question: 'Inside which HTML element do we put the JavaScript?',
@@ -23,12 +9,12 @@ var questionsArray = [
     },
     {
         question: 'Where is the correct place to insert a JavaScript?',
-        answers: ["The <head> section", "Both the <head> section and the <body> section are correct", "The <body> section"],
+        answers: ["The <head> section", "Both the <head> section and the <body> section are correct", "The <body> section", "in the <footer>"],
         correctAnswer: "Both the <head> section and the <body> section are correct"
     },
     {
         question: "How do you create a function in JavaScript?",
-        answers: ["function myFunction()", "function:myFunction()",  "function = myFunction()"],
+        answers: ["function myFunction()", "function:myFunction()",  "function = myFunction()", "(myFunction)"],
         correctAnswer: "function myFunction()"
     },
     {
@@ -42,129 +28,185 @@ var questionsArray = [
         correctAnswer: "for (i = 0; i <= 5; i++)"
         
     }
-]
+];
 
-// start quiz by clicking "Start Quiz!"
-startEl.addEventListener("click", function(){
-    startQuiz();
-}); 
+// get DOM variables
+const timerEl = document.getElementById("time");
+const questionsEl = document.querySelector("questions");
+const questionEl = document.getElementById("question");
+const answersEl = document.querySelector("#answers");
+const notifyEl = document.querySelector("#notify-user");
+const highscoresBtn = document.getElementById("highscores-btn");
+const highscoresSection = document.getElementById("highscores-screen");
+
+// create state object 
+const state = {
+    timeRemaining: 60,
+    questionIndex: 0,
+    userInitials: "",
+    timeId: null,
+    highScores: [],
+  };
 
 
-function startQuiz() {
-    // hides intro page
-    var startScreenEl = document.getElementById("intro");
-    startScreenEl.setAttribute("class", "hide");
-    // displays questions screen 
-    var questionsEl = document.getElementById("questions-page");
-    questionsEl.removeAttribute("class");
-
-    timeRemaining = time;
-    // start timer
-    timeId = setInterval(startTimer, 1000);
-    timerEl.textContent = time;
-
+// initialize the quiz
+const startQuiz = () => {
+    hideElement(document.getElementById("intro"));
+    showElement(document.getElementById("questions-page"));
+  
+    state.timeRemaining = 60;
+    state.timeId = setInterval(startTimer, 1000);
+    timerEl.textContent = state.timeRemaining;
+  
     getQuestions();
-}
+  };
 
-var questionIndex = 0;
-function getQuestions() {
-    // display question from object in array
-    var currentQuestion = questionsArray[questionIndex];
-    //promts new question
-    var questEl = document.getElementById("question");
-    questEl.textContent = currentQuestion.question;
+ 
 
-    // removes previous questions 
+  
+  // add an event listener to the start button to listen for a click event
+startEl.addEventListener("click", startQuiz);
+
+  
+  // display question and answer choices
+  const getQuestions = () => {
+    const currentQuestion = questionsArray[state.questionIndex];
+  
+    questionEl.textContent = currentQuestion.question;
+  
     answersEl.innerHTML = "";
-
-    // calls function for each element in the questionsArray
-    currentQuestion.answers.forEach(function(answer, i) {
-        var choiceClick = document.createElement("button");
-        choiceClick.setAttribute("class", "answer");
-        choiceClick.setAttribute("value", answer);
-        choiceClick.textContent = (i + 1 + ". " + answer);
-        // add click event to each answer choice 
-        choiceClick.onclick = checkAnswer;
-        answersEl.appendChild(choiceClick);
+  
+    currentQuestion.answers.forEach((answer, i) => {
+      const choiceClick = document.createElement("button");
+      choiceClick.setAttribute("class", "answer");
+      choiceClick.setAttribute("value", answer);
+      choiceClick.textContent = `${i + 1}. ${answer}`;
+      choiceClick.onclick = checkAnswer;
+      answersEl.appendChild(choiceClick);
     });
+  };
 
-}
-
-function checkAnswer(){
-    if (this.value !== questionsArray[questionIndex].correctAnswer) {
-        notifyEl.textContent = "Incorrect"
-        // 5 second time penalty for incorrect answers
-        time -= 5;
-    } else {
-        notifyEl.textContent = "Correct!"
+  const checkAnswer = (event) => {
+    const userAnswer = event.target.value;
+    const correctAnswer = questionsArray[state.questionIndex].correctAnswer;
+    const isCorrect = userAnswer === correctAnswer;
+  
+    // Show the notification message
+    const notifyMessage = isCorrect ? "Correct!" : "Incorrect";
+    notifyEl.textContent = notifyMessage;
+    notifyEl.style.display = "block";
+  
+    // Deduct 5 seconds if the answer is incorrect
+    if (!isCorrect) {
+      state.timeRemaining -= 5;
+      timerEl.textContent = state.timeRemaining;
     }
-    // next question
-    questionIndex++;
-    // next question or end quiz 
-    if (questionIndex === questionsArray.length) {
+  
+    // Update the state and render the next question
+    state.questionIndex++;
+    setTimeout(() => {
+      notifyEl.style.display = "none";
+      if (state.questionIndex === questionsArray.length) {
         endQuiz();
-    } else {
+      } else {
         getQuestions();
+      }
+    }, 1000);
+  };
+  
+  const startTimer = () => {
+    state.timeRemaining--;
+    timerEl.textContent = state.timeRemaining;
+    if (state.timeRemaining <= 0) {
+      endQuiz();
+    } else if (notifyEl.textContent === "Incorrect") {
+      state.timeRemaining -= 5;
+      timerEl.textContent = state.timeRemaining;
     }
-}
+  };
+  
 
 
-function endQuiz(){
-    // stop timer
-    clearInterval(timeId);
-    // hides questions screen
-    let questionsScreenEl = document.getElementById("questions-page");
-    questionsScreenEl.setAttribute("class", "hide");
-    // displays finish screen
-    let quizFinishEl = document.getElementById("finish-screen");
-    quizFinishEl.removeAttribute("class");
-};
-
-// when user clicks 'submit' button, high scores screen is displayed
-
-document.getElementById("submit").addEventListener("click", displayHighScores);
-
-
-//after all questions are answered and user submits score, display high scores screen
-function displayHighScores() {
-    // hides finish screen
-    quizFinishEl = document.getElementById("finish-screen");
-    quizFinishEl.setAttribute("class", "hide");
-    // displays high scores screen
-    let highScoresScreenEl = document.getElementById("highscores-screen");
-    highScoresScreenEl.removeAttribute("class");
+const endQuiz = () => {
+    state.score = state.timeLeft;
+    hideElement(document.getElementById("questions-page"));
+    showElement(document.getElementById("finish-screen"));
    
+  };
+  
+
+  const saveHighScore = () => {
+    const initials = document.getElementById("initials-input").value;
+    const score = state.timeRemaining;
+
+    const highScore = {
+        initials, 
+        score
+    };
+
+    let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+    highScores.push(highScore);
+    highScores.sort((a,b) => b.score - a.score);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+
+    showHighScores();
 };
 
 
-// timer counts down and stops when reaches zero
-var startTimer = function() {
-    time--;
-    timerEl.textContent = time;
+const showHighScores = () => {
+    hideElement(document.getElementById("start-screen"));
+    hideElement(document.getElementById("questions-page"));
+    hideElement(document.getElementById("finish-screen"));
+    showElement(document.getElementById("highscores-screen"));
 
-    if(time <= 0) {
-        endQuiz();
+    const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+    const table = document.createElement("table");
+    
+    
+    const tbody = document.createElement("tbody");
+    highScores.forEach(highScore => {
+        const tr = document.createElement("tr");
+        tr.innerHTML = `<td>${highScore.initials}</td> <td>${highScore.score}</td>`;
+        tbody.appendChild(tr);
+    });
+    table.appendChild(tbody);
+    document.getElementById("highscores-list").innerHTML = "";
+    document.getElementById("highscores-list").appendChild(table);
+};
+
+
+highscoresBtn.addEventListener("click", showHighScores);
+
+
+
+const submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", function() {
+    saveHighScore();
+    showHighScores();
+});
+
+
+
+const resetHighScores = () => {
+    localStorage.removeItem("highScores");
+    showHighScores();
+};
+
+const hideElement = element => {
+    if (element) {
+        element.style.display = "none";
     }
 };
 
-// log score and intials to local storage
-let highscore = {
-    initials: userInitials,
-    score: time
+
+const showElement = element => {
+    if (element) {
+        element.style.display = "block";
+    }
 };
-
-let highscoreConvert = JSON.stringify(highscore);
-localStorage.setItem("highscore", highscoreConvert);
-
-let highscoreObj = JSON.parse(localStorage.getItem("highscore"));
-console.log(highscoreObj);
-
-//TODO
-// initals and time
-// print highscores to screen 
-// 'view high scores' button display saved scores 
-// correct/incorrect alert needs to disappear when quiz ends
-
 
 
 
